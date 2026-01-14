87dc9fdca5df681a7b0a31da718d69d6e6c9a86b
finopslatam-sudo <finopslatam@gmail.com>
2026-01-14 18:23:41 -0300

    corregir agregar usuario

--- src/models/subscription.py
+++ src/models/subscription.py
@@ -3,20 +3,22 @@ from datetime import datetime
 from enum import Enum
 
 class ClientSubscription(db.Model):
-    __tablename__ = "client_subscriptions"
+    __tablename__ = 'client_subscriptions'
 
     id = db.Column(db.Integer, primary_key=True)
 
-    client_id = db.Column(
+    user_id = db.Column(
         db.Integer,
-        db.ForeignKey("clients.id"),
         nullable=False
     )
 
     plan_id = db.Column(
         db.Integer,
-        db.ForeignKey("plans.id"),
         nullable=False
     )
 
-    is_active = db.Column(db.Boolean, default=True)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
